77e5d73764cd867298a3cc609effdcd6c4a8a5ae
Emeric Boyadjian <72607059+Boyadjie@users.noreply.github.com>
2024-06-23 13:57:07 +0200

    feat(design): add final design (#5)
    * feat(design): start final design
    
    * feat(player): add animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,21 @@ var currentAreaId = null
 
 signal playInstrument(instrument)
 
+func animate_on_move(velocity):
+	var isHorizontalyMoved = abs(velocity.x) > abs(velocity.y)
+	if isHorizontalyMoved:
+		if velocity.x < 0:
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("Walk_side")
+		elif velocity.x > 0:
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("Walk_side")
+	else:
+		if velocity.y < 0:
+			$AnimatedSprite2D.play("Walk_back")
+		elif velocity.y > 0:
+			$AnimatedSprite2D.play("Walk_front")
+
 func _ready():
 	screen_size = get_viewport_rect().size
 
@@ -19,12 +34,12 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-
+	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+		animate_on_move(velocity)
 	else:
-		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("Stand")
 		
 	if Input.is_action_pressed("sprint"):
 		velocity *= 2
